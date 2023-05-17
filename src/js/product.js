e8afdb4c6e671d92dccf97322f6831ba3a1f6b23
jens747 <jjens7113@gmail.com>
2023-05-16 20:13:16 -0600

    updating branch

--- src/js/product.js
+++ src/js/product.js
@@ -15,3 +15,17 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  // remove from cart button event handler
+async function removeFromCartHandler(e) {
+  // const product = await findProductById(e.target.dataset.id);
+  // addProductToCart(product);
+  if (e.target.classList.contains("cart-card")) {
+    console.log("Delete!")
+  }
+}
+
+// add listener to Remove from Cart button
+document
+  .getElementById("productList")
+  .addEventListener("click", removeFromCartHandler);
