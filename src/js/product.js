623a2b56445d64f6fa5cedb3e79f09fa1a9f5650
jens747 <jjens7113@gmail.com>
2023-05-21 02:18:44 -0600

    completed remove from cart feature

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,26 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, setLocalStorage } from "./utils.mjs";
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
+
+// import { findProductById } from "./productData.mjs";
 
 loadHeaderFooter();
 const productId = getParam("product");
 productDetails(productId);
+
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
+
+// // add to cart butteon event handler
+// async function addToCartHandler(e) {
+//   const product = await findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click",
+//     addToCartHandler
+// );
